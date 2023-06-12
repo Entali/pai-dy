aced8ec0aeb0ed14d4d37fa2eb46725cd913f06b
Nata Entali <nata.entali@protonmail.com>
2023-06-12 18:25:26 +0800

    Text

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react'
+import './Text.scss'
+
+interface Props extends React.HTMLAttributes<HTMLParagraphElement> {
+  children: ReactNode
+}
+
+const Text: React.FC<Props> = (props: Props) => {
+  const { children } = props
+
+  return (
+      <p className="text">{children}</p>
+  )
+}
+
+export default Text;
